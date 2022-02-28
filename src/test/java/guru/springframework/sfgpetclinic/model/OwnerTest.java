7d087842098441c9195829a02afd99c5446fe9cd
Anant Tripathi <ianantktripathi@gmail.com>
2022-02-28 14:07:51 +0530

    Dependent assertion in Order class, See test case for more clarifications

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic.model;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+class OwnerTest {
+    @Test
+    void  dependentAssertion(){
+        Owner owner=new Owner(1l,"Anant","Tripathi");
+        owner.setCity("Gorakhpur");
+        owner.setTelephone("8880494606");
+        assertAll("properties test",
+                ()->assertAll("person properties:",
+                        ()->assertEquals("Anant",owner.getFirstName()),
+                        ()->assertEquals("Tripathi",owner.getLastName())),
+                ()->assertAll("owner properties::",
+                        ()->assertEquals("Gorakhpur",owner.getCity()),
+                        ()->assertEquals("8880494606",owner.getTelephone())));
+                }
+    }
--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -6,8 +6,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
 
-
-
     @Test
     void groupedAsserstions() {
         //given
